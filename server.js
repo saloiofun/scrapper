af7fb33ca053578bb46370cf5f85d8f9686086bf
Sandro Wong <saloiofun@gmail.com>
2017-10-23 10:22:09 -0700

    Remove comments

--- server.js
+++ server.js
@@ -79,24 +79,20 @@ app.get("/scrape", function (req, res) {
 
 // Route for getting all Articles from the db
 app.get("/", function (req, res) {
-    // Grab every document in the Articles collection
     db.Article
         .find({})
         .then(function (dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
             res.render("index", {
                 articles: dbArticle
             });
         })
         .catch(function (err) {
-            // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
 // Route for getting all Saved Articles from the db
 app.get("/saved", function (req, res) {
-    // Grab every document in the Articles collection
     db.Article
         .find({
             saveArticle: {
@@ -104,13 +100,11 @@ app.get("/saved", function (req, res) {
             }
         })
         .then(function (dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
             res.render("saved", {
                 savedArticles: dbArticle
             });
         })
         .catch(function (err) {
-            // If an error occurred, send it to the client
             res.json(err);
         });
 });
@@ -143,19 +137,15 @@ app.post("/save/:id", function (req, res) {
 
 // Route for grabbing a note
 app.get("/saved/note/:id", function (req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article
         .findOne({
             _id: req.params.id
         })
-        // ..and populate all of the notes associated with it
         .populate("note")
         .then(function (dbArticle) {
-            // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
         })
         .catch(function (err) {
-            // If an error occurred, send it to the client
             res.json(err);
         });
 });
